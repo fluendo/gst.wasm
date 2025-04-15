0e40b637c8b19e565758ab93f50cb140661f32f6
Alexander Slobodeniuk <aslobodeniuk@fluendo.com>
2025-04-15 18:10:16 +0200

    avdec: sample: avoid initialization of the webcodecs
    + reduce the size of the final blob
    
    Issue: RDI_2454

--- gst.wasm/subprojects/samples/codecs-avdec-h264/codecs-avdec-h264-example.c
+++ gst.wasm/subprojects/samples/codecs-avdec-h264/codecs-avdec-h264-example.c
@@ -21,6 +21,8 @@
  */
 
 #include <gst/emscripten/gstemscripten.h>
+#include <gst/web/gstwebutils.h>
+#include <gst/web/gstwebvideoframe.h>
 
 GST_DEBUG_CATEGORY_STATIC (example_dbg);
 #define GST_CAT_DEFAULT example_dbg
@@ -30,19 +32,24 @@ static GstElement *pipeline;
 static void
 register_elements ()
 {
-  GST_PLUGIN_STATIC_DECLARE (coreelements);
-  GST_PLUGIN_STATIC_DECLARE (web);
-  GST_PLUGIN_STATIC_DECLARE (opengl);
-  GST_PLUGIN_STATIC_DECLARE (isomp4);
+  GST_ELEMENT_REGISTER_DECLARE (queue);
+  GST_ELEMENT_REGISTER_DECLARE (qtdemux);
+  GST_ELEMENT_REGISTER_DECLARE (glimagesink);
+  GST_ELEMENT_REGISTER_DECLARE (web_canvas_sink);
+  GST_ELEMENT_REGISTER_DECLARE (web_stream_src);
+  GST_ELEMENT_REGISTER_DECLARE (videoconvert);
   GST_PLUGIN_STATIC_DECLARE (libav);
-  GST_PLUGIN_STATIC_DECLARE (videoconvertscale);
 
-  GST_PLUGIN_STATIC_REGISTER (coreelements);
-  GST_PLUGIN_STATIC_REGISTER (web);
-  GST_PLUGIN_STATIC_REGISTER (opengl);
-  GST_PLUGIN_STATIC_REGISTER (isomp4);
+  gst_web_utils_init ();
+  gst_web_video_frame_init ();
+
   GST_PLUGIN_STATIC_REGISTER (libav);
-  GST_PLUGIN_STATIC_REGISTER (videoconvertscale);
+  GST_ELEMENT_REGISTER (web_canvas_sink, NULL);
+  GST_ELEMENT_REGISTER (web_stream_src, NULL);
+  GST_ELEMENT_REGISTER (glimagesink, NULL);
+  GST_ELEMENT_REGISTER (qtdemux, NULL);
+  GST_ELEMENT_REGISTER (queue, NULL);
+  GST_ELEMENT_REGISTER (videoconvert, NULL);
 }
 
 static void
